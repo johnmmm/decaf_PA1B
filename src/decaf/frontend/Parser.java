26b0da7431e862f61d240d96c527899ba0940015
周京汉 <zhoujh15@mails.tsinghua.edu.cn>
2017-11-06 22:06:16 +0800

    try to use BEGIN&END collection.

--- src/decaf/frontend/Parser.java
+++ src/decaf/frontend/Parser.java
@@ -79,7 +79,32 @@ public class Parser extends Table {
      * @return the parsed value of `symbol` if parsing succeeded, otherwise `null`.
      */
     private SemValue parse(int symbol, Set<Integer> follow) {
-        Pair<Integer, List<Integer>> result = query(symbol, lookahead); // get production by lookahead symbol
+        Pair<Integer, List<Integer>> result = null; // get production by lookahead symbol
+        
+        Set<Integer> begin = beginSet(symbol);
+        Set<Integer> end = new HashSet<>();
+        end.addAll(follow);
+   		end.addAll(followSet(symbol));
+   		
+   		result = query(symbol, lookahead); // get production by lookahead symbol
+        if(result == null) {
+	        	error();
+	        	while(true) {
+	        		if(begin.contains(lookahead)) 
+	        		{
+	        			return parse(symbol, end);
+	        		}
+	        		else if(end.contains(lookahead)) 
+	        		{
+	        			return null;
+	        		}
+	        		else
+	        		{
+	        			lookahead = lex();
+	        		}
+	        	}
+        }
+        
         int actionId = result.getKey(); // get user-defined action
 
         List<Integer> right = result.getValue(); // right-hand side of production
@@ -95,7 +120,7 @@ public class Parser extends Table {
         }
 
         params[0] = new SemValue(); // initialize return value
-        act(actionId, params); // do user-defined action
+        	act(actionId, params); // do user-defined action
         return params[0];
     }
 
